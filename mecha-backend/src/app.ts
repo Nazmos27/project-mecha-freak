29ba4b76f817179543167002916c8d03f2f1c559
Nazmos27 <nazmos789@gmail.com>
2024-09-17 12:36:02 +0600

    backend packeges installed

--- mecha-backend/src/app.ts
+++ mecha-backend/src/app.ts
@@ -1,3 +1,22 @@
-console.log('hello world from app.ts');
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import router from './app/routes';
+import notFound from './app/middlewares/notFound';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 
-console.log('checking');
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+//application routes
+app.use('/api', router);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.use(globalErrorHandler);
+app.use(notFound);
+
+export default app;
